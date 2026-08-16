7f2cbef80e10ab3105ad09185110788f167c72d7
0xCLWN <277256795+0xCLWN@users.noreply.github.com>
2026-08-16 10:57:36 +0300

    chore: bump version to 1.3.2

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
-val appVersion = "1.3.1"
+val appVersion = "1.3.2"
 
 // Usage: ./gradlew :composeApp:run -PbakedKeys="vless://key1#Name1,vless://key2#Name2"
 val bakedKeys: String = findProperty("bakedKeys")?.toString() ?: ""
